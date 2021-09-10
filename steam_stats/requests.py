8a62eff819447f774dd978c6060ae6bb97a4d965
dbeley <6568955+dbeley@users.noreply.github.com>
2021-09-10 20:49:14 +0200

    Update steam_stats.

--- steam_stats/requests.py
+++ steam_stats/requests.py
@@ -1,10 +1,26 @@
 import requests
+import logging
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+
+logger = logging.getLogger(__name__)
+
+
+def get_steam_json(url, appid):
+    s = requests.Session()
+    logger.debug(f"get_json {url}")
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
+    s.mount("http://", HTTPAdapter(max_retries=retries))
+    s.mount("https://", HTTPAdapter(max_retries=retries))
+    result = s.get(url)
+    if result.text != "":
+        return result.json()
+    return {str(appid): {"success": False}}
 
 
 def get_json(url):
     s = requests.Session()
+    logger.debug(f"get_json {url}")
     retries = Retry(total=5, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
     s.mount("http://", HTTPAdapter(max_retries=retries))
     s.mount("https://", HTTPAdapter(max_retries=retries))
