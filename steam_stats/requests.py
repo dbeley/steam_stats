a5fe90d24f20c9d57afd8f4be90967b2209adb45
dbeley <6568955+dbeley@users.noreply.github.com>
2023-02-03 00:57:16 +0100

    Handle steam rate-limiting

--- steam_stats/requests.py
+++ steam_stats/requests.py
@@ -1,18 +1,25 @@
 import logging
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+import time
 
 logger = logging.getLogger(__name__)
 
 
 def get_steam_json(s, url, appid):
-    logger.debug(f"get_json {url}")
-    result = s.get(url)
+    sleep_time = 10
+    while True:
+        result = s.get(url)
+        if result.status_code == 429:
+            logger.warning("Rate-limit detected, waiting for %s seconds.", sleep_time)
+            time.sleep(sleep_time)
+            sleep_time += 5
+        else:
+            break
     if result.text != "":
         return result.json()
     return {str(appid): {"success": False}}
 
 
 def get_json(s, url):
-    logger.debug(f"get_json {url}")
     return s.get(url).json()
